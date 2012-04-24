48cc03cb4b0c13829232feb95a58376feeb074b8
fuqcool <fuqcool@gmail.com>
2012-04-24 22:38:39 +0800

    Check HOME first, if not exists, use APPDATA as emacs home.

--- setup.bat
+++ setup.bat
@@ -1,3 +1,6 @@
-mklink %USERPROFILE%\.emacs %~dp0.emacs
-mklink /D %USERPROFILE%\.emacs.d %~dp0.emacs.d
+@echo off
+if defined HOME (set emacs_home=%HOME%) else (set emacs_home=%APPDATA%)
+
+mklink %emacs_home%\.emacs %~dp0.emacs
+mklink /D %emacs_home%\.emacs.d %~dp0.emacs.d
 pause
