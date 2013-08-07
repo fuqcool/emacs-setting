86236a41e14759df921a7aba59b1883bf676956b
fuqcool <fuqcool@gmail.com>
2013-08-07 15:28:36 +0800

    split files

---
+++ .emacs.d/key-bindings.el
@@ -0,0 +1,52 @@
+;; shortcuts
+
+;; f1 -> shell
+(global-set-key [f1] 'shell)
+
+;; f12 -> open .emacs
+(global-set-key [f12] (lambda ()
+                        (interactive)
+                        (find-file "~/.emacs")))
+
+;; Click C-a go to beginning of line, click again
+;; go to beginning of line text.
+(global-set-key "\C-a"
+                (lambda ()
+                  (interactive)
+                  (if (= (point) (line-beginning-position))
+                      (beginning-of-line-text)
+                    (beginning-of-line))))
+
+;; kill whole line
+(global-set-key "\C-k" 'kill-whole-line)
+
+;; \C-; => comment or uncomment line
+(global-set-key [(control ?\;)]
+                (lambda ()
+                  (interactive)
+                  (comment-or-uncomment-region (line-beginning-position)
+                                               (line-end-position))))
+
+;; copy region or whole line
+(global-set-key "\M-w"
+                (lambda ()
+                  (interactive)
+                  (if mark-active
+                      (kill-ring-save (region-beginning)
+                                      (region-end))
+                    (progn
+                      (kill-ring-save (line-beginning-position)
+                                      (line-end-position))
+                      (message "copied line")))))
+
+;; kill region or whole line
+(global-set-key "\C-w"
+                (lambda ()
+                  (interactive)
+                  (if mark-active
+                      (kill-region (region-beginning)
+                                   (region-end))
+                    (progn
+                      (kill-region (line-beginning-position)
+                                   (line-end-position))
+                      (message "killed line")))))
---
+++ .emacs.d/win-init.el
@@ -0,0 +1,35 @@
+;; hide tool bar
+(tool-bar-mode 0)
+
+;; Iterate a list of fonts, if current font exists, set
+;; font style and size.
+(defun try-set-font (font-list)
+  (defun font-exists (font-name)
+    (if (null (x-list-fonts font-name))
+        nil t))
+  (when font-list
+    (let ((font-name (car (car font-list)))
+          (font-size (cdr (car font-list))))
+      (if (font-exists font-name)
+          (set-default-font (concat font-name "-"
+                                    (number-to-string font-size)))
+        (try-set-font (cdr font-list))))))
+
+(try-set-font '(("Monaco" . 17)
+                ("Consolas" . 13)
+                ("DejaVuSans Mono" . 12)
+                ("Courier New" . 12)))
+
+;; color theme
+(add-to-list 'load-path "~/.emacs.d/plugins/color-theme")
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-robin-hood)
+
+(linum-mode)
+
+(add-to-list 'load-path "~/.emacs.d/plugins/flymake")
+;; show error message in mini buffer
+(require 'flymake-cursor)
+;; show errors on fringe
+(require 'rfringe)
