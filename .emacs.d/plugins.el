aca38acff367eff8b4378d295f080cdbd832ee2a
fuqcool <fuqcool@gmail.com>
2013-08-08 14:49:45 +0800

    reorgnize

---
+++ .emacs.d/plugins.el
@@ -0,0 +1,62 @@
+(add-to-list 'load-path "~/.emacs.d/plugins")
+
+;; yasnippet
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas/global-mode 1)
+(setq yas/prompt-functions
+      '(yas/dropdown-prompt
+        yas/completing-prompt))
+
+
+;; dropdown list
+(require 'dropdown-list)
+(set-face-foreground 'dropdown-list-face "black")
+(set-face-background 'dropdown-list-face "lightgray")
+(set-face-foreground 'dropdown-list-selection-face "white")
+(set-face-background 'dropdown-list-selection-face "steelblue")
+
+
+;; google c style
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+;; use c++ mode when openning c/c++ header files
+(add-to-list 'auto-mode-alist '("\\.h$" . c++-mode))
+
+
+;; auto complete
+(add-to-list 'load-path "~/.emacs.d/plugins/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/plugins/auto-complete/dict")
+(ac-config-default)
+;; Use C-n, C-p to navigate candidates.
+(setq ac-use-menu-map t)
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
+;; Manually trigger auto complete.
+(setq ac-auto-start nil)
+(global-set-key "\M-/" 'ac-start)
+(define-key ac-complete-mode-map "\M-/" 'ac-stop)
+
+
+;; yaml mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
+
+
+(when (display-graphic-p)
+  ;; color theme
+  (add-to-list 'load-path "~/.emacs.d/plugins/color-theme")
+  (require 'color-theme)
+  (color-theme-initialize)
+  (color-theme-robin-hood)
+
+  ;; flymake
+  (add-to-list 'load-path "~/.emacs.d/plugins/flymake")
+  ;; show error message in mini buffer
+  (require 'flymake-cursor)
+  ;; show errors on fringe
+  (require 'rfringe)
+
+  )
---
+++ .emacs.d/settings.el
@@ -0,0 +1,70 @@
+;; share clipboard with external programs
+(setq x-select-enable-clipboard t)
+
+;; insert spaces instead of tabs
+(setq-default indent-tabs-mode nil)
+
+;; prefer utf-8
+(setq prefer-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+
+;; ignore buffer name cases
+(setq read-buffer-completion-ignore-case t)
+;; ignore file name cases
+(setq read-file-name-completion-ignore-case t)
+
+;; find true name of a file
+(setq find-file-visit-truename t)
+(setq find-file-suppress-same-file-warnings t)
+
+;; Use text mode as default major mode.
+(setq default-major-mode 'text-mode)
+
+;; Set title format -- display file name or buffer name.
+(setq frame-title-format '(buffer-file-name "%f" "%b"))
+
+;; Remove trailing whitespaces.
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; open json file in javascript mode
+(add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
+;; open rakefile in ruby mode
+(add-to-list 'auto-mode-alist '("^[Rr]akefile$" . ruby-mode))
+;; js-mode setting
+(setq js-auto-indent-flag nil)
+(setq js-indent-level 2)
+
+(when (display-graphic-p)
+  ;; hide tool bar
+  (tool-bar-mode 0)
+
+  ;; Iterate a list of fonts, if current font exists, set
+  ;; font style and size.
+  (defun try-set-font (font-list)
+    (defun font-exists (font-name)
+      (if (null (x-list-fonts font-name))
+          nil t))
+    (when font-list
+      (let ((font-name (car (car font-list)))
+            (font-size (cdr (car font-list))))
+        (if (font-exists font-name)
+            (set-default-font (concat font-name "-"
+                                      (number-to-string font-size)))
+          (try-set-font (cdr font-list))))))
+
+  (try-set-font '(("Monaco" . 17)
+                  ("Consolas" . 13)
+                  ("DejaVuSans Mono" . 12)
+                  ("Courier New" . 12)))
+
+  ;; show line numbers
+  (global-linum-mode)
+  ;; Always reserve 3-digit-width when line number is less than 100.
+  (setq linum-format "%3d")
+
+  (when (string-equal system-type "darwin")
+    (set-frame-position (selected-frame) 0 0)
+    (set-frame-width (selected-frame)
+                     (- (/ (x-display-pixel-width) (frame-char-width)) 4)))
+
+  )
