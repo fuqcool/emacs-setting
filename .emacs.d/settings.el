93e9e79c75bb851b7c7a810381052875188202f8
fuqcool <fuqcool@gmail.com>
2013-09-14 00:29:50 -0400

    set emacs PATH as shell PATH

--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -35,6 +35,16 @@
 (setq js-auto-indent-flag nil)
 (setq js-indent-level 2)
 
+;; set emacs PATH to shell PATH
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+
 (when (display-graphic-p)
   ;; hide tool bar
   (tool-bar-mode 0)
