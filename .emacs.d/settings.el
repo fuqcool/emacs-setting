b1e0155b54eec5f63a4e9379890e2c698741535b
fuqcool <fuqcool@gmail.com>
2013-09-23 15:40:30 -0400

    remind me when column length exceeds 80 on scheme mode

--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -31,6 +31,8 @@
 ;; open rakefile in ruby mode
 (add-to-list 'auto-mode-alist '("^[Rr]akefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rkt$" . scheme-mode))
+(add-hook 'scheme-mode-hook
+          (lambda () (highlight-lines-matching-regexp ".\\{81\\}" "red")))
 ;; js-mode setting
 (setq js-auto-indent-flag nil)
 (setq js-indent-level 2)
