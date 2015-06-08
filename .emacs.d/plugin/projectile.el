b3ef69dcf2d1fcf635a89d05dd8d3ed72fd5cf76
Chiang Fu <fuqcool@gmail.com>
2015-06-08 17:19:01 -0400

    projectile shortcuts

--- .emacs.d/plugin/projectile.el
+++ .emacs.d/plugin/projectile.el
@@ -1,3 +1,7 @@
 (projectile-global-mode)
 
 (setq projectile-completion-system 'grizzl)
+
+;; Shortcuts
+(global-set-key (kbd "C-c p p") 'projectile-switch-project)
+(global-set-key (kbd "C-c p s") 'projectile-switch-project)
